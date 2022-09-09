09f849b74d0120c0b2462b9129ca761391ffd592
Boula Akladyous <akladyous@gmail.com>
2022-09-09 18:16:01 -0400

    create friendship model to handle following/followers users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :trackable
 
   has_many :posts, dependent: :destroy
-  has_many :likes
+  has_many :likes, dependent: :destroy
+
+  has_many :following_users, class_name: 'Friendship', foreign_key: :follower_id, dependent: :destroy
+  has_many :following, through: :following_users, source: :followed
+  has_many :followed_users, class_name: 'Friendship', foreign_key: :followed_id, dependent: :destroy
+  has_many :followers, through: :followed_users, source: :follower
+
 
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
