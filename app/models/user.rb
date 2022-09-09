1a993987aec8daf9257559ee5cfaf32c92c4e706
Boula Akladyous <akladyous@gmail.com>
2022-09-09 00:05:16 -0400

    create likes joinTable with model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :trackable
 
   has_many :posts, dependent: :destroy
+  has_many :likes
 
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_205841) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_09_035214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,12 +52,33 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_205841) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "posts_topics", id: false, force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "topic_id", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.integer "reviews_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
