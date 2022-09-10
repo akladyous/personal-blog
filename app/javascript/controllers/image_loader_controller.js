8899f6f50f1e401fb9bd514df609214a7d99b46e
Boula Akladyous <akladyous@gmail.com>
2022-09-10 14:25:20 -0400

    completed image_uploader stimulus controller

--- app/javascript/controllers/image_loader_controller.js
+++ app/javascript/controllers/image_loader_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = ["imageContainer", "input", "output"]
     connect() {
+        console.log('connected')
     }
 
     triggerUpload(event) {
