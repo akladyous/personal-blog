951594d7cb648a869aaa7349564fc37ac2f337ae
Boula Akladyous <akladyous@gmail.com>
2022-09-10 12:07:47 -0400

    create image_loader stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import ImageLoaderController from "./image_loader_controller"
+application.register("image-loader", ImageLoaderController)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
 
+  has_one_attached :avatar
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
 
