d0a660d50c88efb9d581daedd4a14e51ad874e74
Boula Akladyous <akladyous@gmail.com>
2022-09-12 19:05:43 -0400

    added user notification to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # namespace :posts do
-  #   resource :tags
-  # end
   resources :posts do
     resource :like, module: :posts, only: [:show, :create, :destroy]
   end
   root 'home#index'
   devise_for :users
+
+
 end
