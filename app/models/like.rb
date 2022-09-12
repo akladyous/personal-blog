2c26b1eee0adc628257a5f9f682a020b8151801a
Boula Akladyous <akladyous@gmail.com>
2022-09-12 16:47:05 -0400

    completed friendship model without requesting friendship

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,4 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  # def likes?(post)
-  #   post.likes.where(user_id: id).any?
-  # end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,17 +15,17 @@ class User < ApplicationRecord
   has_many :followers, through: :followed_users, source: :follower
 
   def follow(user)
+    Friendship.find_or_create_by(follower_id: self.id, followed_id: user.id)
     # following_users.create(followed_id: user.id)
-    Friendship.find_or_create_by(follower_id: current_user.id, followed_id: user.id)
   end
 
   def unfollow(user)
-    Friendship.where(follower_id: current_user.id, followed_id: user.id).destroy_all
-    current_user.following_users.where(followed_id: user.id)
+    Friendship.where(follower_id: self.id, followed_id: user.id).destroy_all
+    # current_user.following_users.where(followed_id: user.id)
   end
 
   def is_following?(user)
-    current_user.following&.include?(user)
+    self.following&.include?(user)
   end
   def full_name
     "#{first_name.capitalize} #{last_name.capitalize}"
