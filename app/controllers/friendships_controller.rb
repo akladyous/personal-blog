bedd09ab431350de4110b2a7a6376dbf2ff98b4e
Boula Akladyous <akladyous@gmail.com>
2022-09-13 13:27:50 -0400

    added request accept reject Friend actions to Friendship_controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,17 +3,23 @@ class FriendshipsController < ApplicationController
   before_action :set_friend
 
   def new
+    current_user.request_friend(@user)
+    redirect_back_or_to root_path
   end
 
   def create
+    current_user.accept_friend(@user)
+    redirect_back_or_to root_path
   end
 
   def destroy
+    current_user.reject_friend(@user)
+    redirect_back_or_to root_path
   end
 
   private
   def set_friend
-    @friend = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 end
 
