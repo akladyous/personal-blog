d97d417f1efc84d7423b7da166f822a2dfb90d27
Boula Akladyous <akladyous@gmail.com>
2022-09-15 17:59:43 -0400

    added commentable routes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,12 @@ class Comment < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :commentable, polymorphic: true
   has_many   :comments, as: :commentable
+
+  def deleted?
+    user.nil?
+  end
+
+  def destroy
+    user(user: nil, content: nil)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
     resource :like, controller: 'posts/likes', only: [:show, :create, :destroy]
     resources :tags, controller: 'posts/tags'
     resources :related, controller: 'posts/related', only: :index
+    resources :comments, only: [:new, :create, :destroy], module: :posts
   end
+
+  resources :comments, only: [] do
+    resources :comments, only: [:new, :create, :destroy], module: :comments
+  end
+
   resources :friendships, only: [:show, :new, :create, :destroy, :update]
   resources :me, only: :show
 
