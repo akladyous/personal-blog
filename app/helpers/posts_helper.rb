b1a805a7e85f301e614ec6a7df90515aeb6c0d97
Boula Akladyous <akladyous@gmail.com>
2022-09-17 14:59:39 -0400

    added custom style to post preview partial

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,9 @@
 module PostsHelper
 
+  def time_to_read(post)
+    average_words_per_minute = 200.00
+    content = post.content.body.to_plain_text.split(' ')
+    word_count = content.split(' ').count
+    average = (word_count / average_words_per_minute).ceil
+  end
 end
