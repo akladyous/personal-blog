6de59c2dc826a504515b85c78925c50a84066b71
Boula Akladyous <akladyous@gmail.com>
2022-09-19 18:23:39 -0400

    added custom formBuilder

--- app/helpers/bootstrap_builder.rb
+++ app/helpers/bootstrap_builder.rb
@@ -1,25 +1,28 @@
 class BootstrapBuilder < ActionView::Helpers::FormBuilder
+  # include ActionView::Helpers::TagHelper
+  delegate :content_tag, :tag, to: :@template
 
-  def text_field(method, options={})
-    super(method, options.merge(class: 'form-control', autocomplete: "off"))
-  end
-  def email_field(method, options={})
-    super(method, options.merge(class: 'form-control', autocomplete: "off"))
-  end
-  def password_field(method, options={})
-    super(method, options.merge(class: 'form-control', autocomplete: "off"))
-  end
   def submit(value, options={})
     super(value, options.merge(class: 'btn btn-light rounded-pill py-2 px-4'))
   end
 
-  def label(method, text = nil, options = {})
-    super(method, options.merge(class: 'form-label', autocomplete: "off"))
+  def label(method, options = {})
+    super(method, options.merge(class: 'form-label'))
   end
 
   def form_container
     @template.content_tag :div, nil, class: "form-group mb-2" do
-
     end
   end
+
+  %i[text_field text_area email_field password_field].each do |method_name|
+    define_method(method_name) do |method, options = {}|
+      super(method, options.merge(class: 'form-control', autocomplete: 'off'))
+      # content_tag :div, class: 'form-group mb-2' do
+      #   label method, class: 'form-label'
+      #   super(method, options.merge(class: 'form-control', autocomplete: 'off'))
+      # end
+    end
+  end
+
 end
