189fc7258219d43a5e5af6a78f2cb110d3452ae6
Boula Akladyous <akladyous@gmail.com>
2022-09-21 15:17:24 -0400

    added virtual attribute to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,12 @@ class User < ApplicationRecord
   has_many :comments
 
   def full_name
-    "#{first_name.capitalize} #{last_name.capitalize}"
+    [first_name, last_name].join(' ')
+  end
+  def full_name=(name)
+    split = name.split(' ', 2)
+    self.first_name = split.first
+    self.last_name = split.last
   end
 
   # delegate :likes?, to: :posts
