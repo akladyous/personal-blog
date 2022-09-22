2ca8b57a888748639f2c0d0f9600c49e4c064284
Boula Akladyous <akladyous@gmail.com>
2022-09-22 16:14:43 -0400

    added posts to home page

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,6 @@ class Tag < ApplicationRecord
   has_many :posts, through: :taggables
 
   validates :name, presence: true
+
+  scope :top10, -> { left_joins(:posts).merge(Post.order(view_count: :desc).limit(10)).pluck(:name) }
 end
