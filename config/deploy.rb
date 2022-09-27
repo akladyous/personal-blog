ebc6c3d9fa468d0351a5c7a8fe4798ddd4d44330
Boula Akladyous <akladyous@gmail.com>
2022-09-27 15:38:26 -0400

    restored masterkey

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,6 @@
-set :default_env, {
-  'RAILS_ENV' => 'production',
-  'RAILS_MASTER_KEY' => ENV['RAILS_MASTER_KEY']
-}
+# set :default_env, {
+#   'RAILS_ENV' => 'production',
+#   'RAILS_MASTER_KEY' => ENV['RAILS_MASTER_KEY']
+# }
+
+append :linked_files, 'config/master.key'
