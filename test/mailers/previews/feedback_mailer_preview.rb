ec7c1469673c7ade2f16c40db5d07a8ebc13ece1
Boula Akladyous <akladyous@gmail.com>
2022-10-17 23:13:51 -0400

    removed test unit

--- test/mailers/previews/feedback_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/feedback_mailer
-class FeedbackMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/feedback_mailer/send_feedback
-  def send_feedback
-    FeedbackMailer.send_feedback
-  end
-
-end
